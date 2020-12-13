5f025659e767a37d6b8630b4d91054ff3c216fb2
tudr <70108891+tt657@users.noreply.github.com>
2020-12-13 18:02:14 +0200

    First

---
+++ A.cpp
@@ -0,0 +1,53 @@
+#includes pointer 
+#incldes (true) 
+#includes (falsename) 
+private void (true) 
+void (false) 
+void (falsename) 
+false void 
+private falsename (true) 
+false falsename (true) 
+farcalloc (true) 
+read *
+public void 
+pointer truename * 
+public [1++]
+name true [1] [2+]
+true [1] [3] [++] 
+time false (true) [1] [5] [+] 
+true falsename [1] [3] [0+]
+truename [1] [5]
+namespace [0] 
+goto 7 
+true [6] [1] [0]
+goto [4] 
+void (truename) 
+public void (true)
+read * 
+void (private) 
+namespace [7] [0] 
+true (void) 
+vector = 0 
+false = [3]
+vector (true) 
+void (true) 
+false void [3] [2]
+void true 
+namespace [2] [0]
+void *  (truename) 
+read * 
+virtual (0) 
+void (9)) 
+goto 1 
+name [2] [4]
+pointer (2) 
+true void
+void (2)
+goto (0)
+virtual [6] [5]
+read * 
+void public  (true) 
+goto 01 
+virtual = 0 
+vector [10000]
+std::vector< = -- - s> v;
